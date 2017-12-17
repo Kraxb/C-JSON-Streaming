df1765d49a7b8974002ebcbd1df9901baf687153
Kraxb <34626190+Kraxb@users.noreply.github.com>
2017-12-17 21:18:21 +0100

    Update Serialization.c

--- Serialization.c
+++ Serialization.c
@@ -7,14 +7,13 @@ unsigned int StructureSize(const SerializationInfo_t info){
 }
 
 int FindParameterInfo(const SerializationInfo_t info, char *str , int len, int *ParType, int *ParOffset, int *ParSize){
-  int pos=1;
-
-  while(info[pos][0]!=0){
-     if (!strlen((char*)info[pos][0]) == len && strncmp((char*)info[pos][0],str,len)){
-       
-       if(ParType!=0)   *ParType   = (unsigned int)info[pos][1];
-       if(ParOffset!=0) *ParOffset = (unsigned int)info[pos][2];
-       if(ParSize!=0)   *ParSize   = (unsigned int)info[pos][3];
+  for(int i=1; info[i][0]!=0;i++){
+     
+     if (strlen((char*)info[i][0]) == len && !strncmp((char*)info[i][0],str,len)){ 
+     
+       if(ParType!=0)   *ParType   = (unsigned int)info[i][1];
+       if(ParOffset!=0) *ParOffset = (unsigned int)info[i][2];
+       if(ParSize!=0)   *ParSize   = (unsigned int)info[i][3];
        return 0;
      }
   }
@@ -23,7 +22,7 @@ int FindParameterInfo(const SerializationInfo_t info, char *str , int len, int *
 
 int GetParameterInfo(const SerializationInfo_t info, int Index, char **name, int *ParType, int *ParOffset, int *ParSize)
 {
-    if (info[Index][0] != 0)
+    if (info[Index][0] == 0)
         return 1;
 
     if (name != 0)      *name      = (char  *) (unsigned int)info[Index][0];
